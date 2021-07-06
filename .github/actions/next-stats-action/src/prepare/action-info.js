7b8b38c28f97d0e8de3c397c3dc6a924d4bf0014
colin zheng <colinzheng423@gmail.com>
2021-07-06 05:41:17 -0700

    add commit

--- .github/actions/next-stats-action/src/prepare/action-info.js
+++ .github/actions/next-stats-action/src/prepare/action-info.js
@@ -28,7 +28,7 @@ module.exports = function actionInfo() {
     const parentDir = path.join(cwd, '../..')
 
     if (!GITHUB_REF) {
-      // get the current branch name
+      // get the current branch name ex: master 
       GITHUB_REF = execSync(`cd "${cwd}" && git rev-parse --abbrev-ref HEAD`)
         .toString()
         .trim()
